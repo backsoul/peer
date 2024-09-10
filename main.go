bf5780f40f673319a12d278d5313416bf311d454
Daniel Sarmiento <daniel.sarmiento@dafiti.com.co>
2024-09-10 16:20:57 -0500

    commit

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
@@ -13,7 +14,7 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-var rooms = make(map[string]map[*websocket.Conn]bool)
+var rooms = make(map[string]map[*websocket.Conn]string) // map connection to UUID
 
 func main() {
 	http.HandleFunc("/", handleConnection)
@@ -33,7 +34,14 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	fmt.Println("Client connected")
+	clientUUID := uuid.New().String() // Generate a UUID for the client
+	fmt.Println("Client connected with UUID:", clientUUID)
+
+	// Send the UUID to the client
+	conn.WriteMessage(websocket.TextMessage, encodeJSON(map[string]interface{}{
+		"type": "uuid",
+		"uuid": clientUUID,
+	}))
 
 	for {
 		_, message, err := conn.ReadMessage()
@@ -56,38 +64,24 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 
 		switch messageType {
 		case "join":
-			handleJoin(conn, data)
-		case "start_call":
-			handleRoomMessage(data, conn)
-		case "webrtc_offer":
-			handleRoomMessage(data, conn)
-		case "webrtc_answer":
-			handleRoomMessage(data, conn)
-		case "webrtc_ice_candidate":
-			handleRoomMessage(data, conn)
+			handleJoin(conn, data, clientUUID)
+		case "start_call", "webrtc_offer", "webrtc_answer", "webrtc_ice_candidate":
+			handleRoomMessage(data, conn, clientUUID)
 		default:
 			log.Printf("Unknown message type: %s\n", messageType)
 		}
 	}
 }
 
-func handleJoin(conn *websocket.Conn, data map[string]interface{}) {
+func handleJoin(conn *websocket.Conn, data map[string]interface{}, clientUUID string) {
 	roomID, _ := data["roomId"].(string)
 	room, exists := rooms[roomID]
 	if !exists {
-		room = make(map[*websocket.Conn]bool)
+		room = make(map[*websocket.Conn]string)
 		rooms[roomID] = room
 	}
 
-	// if len(room) >= 2 {
-	// 	conn.WriteMessage(websocket.TextMessage, encodeJSON(map[string]interface{}{
-	// 		"type":   "full_room",
-	// 		"roomId": roomID,
-	// 	}))
-	// 	return
-	// }
-
-	room[conn] = true
+	room[conn] = clientUUID
 
 	if len(room) == 1 {
 		conn.WriteMessage(websocket.TextMessage, encodeJSON(map[string]interface{}{
@@ -101,27 +95,32 @@ func handleJoin(conn *websocket.Conn, data map[string]interface{}) {
 		}))
 		broadcast(roomID, map[string]interface{}{
 			"type": "start_call",
-		})
+		}, clientUUID)
 	}
 }
 
-func handleRoomMessage(data map[string]interface{}, conn *websocket.Conn) {
+func handleRoomMessage(data map[string]interface{}, conn *websocket.Conn, clientUUID string) {
 	roomID, _ := data["roomId"].(string)
 	room, exists := rooms[roomID]
 	if exists {
-		for client := range room {
+		for client, _ := range room {
 			if client != conn {
-				client.WriteMessage(websocket.TextMessage, encodeJSON(data))
+				message := data
+				message["from"] = clientUUID // Add the sender's UUID
+				client.WriteMessage(websocket.TextMessage, encodeJSON(message))
 			}
 		}
 	}
 }
 
-func broadcast(roomID string, message map[string]interface{}) {
+func broadcast(roomID string, message map[string]interface{}, clientUUID string) {
 	room, exists := rooms[roomID]
 	if exists {
-		for client := range room {
-			client.WriteMessage(websocket.TextMessage, encodeJSON(message))
+		for client, uuid := range room {
+			if uuid != clientUUID {
+				message["from"] = clientUUID // Include the sender's UUID
+				client.WriteMessage(websocket.TextMessage, encodeJSON(message))
+			}
 		}
 	}
 }
